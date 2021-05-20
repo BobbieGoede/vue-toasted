20ac3cf1ba0b175cdbe09ef5de8960b34032df91
Bobbie Goede <bobbiegoede@gmail.com>
2021-05-20 10:07:51 +0200

    Change index imports and typing

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-import { Toasted as T } from "./js/toast";
-// @ts-ignore-next-line
+import { Toasted } from "./js/toast";
 import ToastComponent from "./toast.vue";
+import { ToastOptions } from "./js/show";
+import { PluginObject } from "vue/types/umd";
 
-const Toasted = {
+const ToastedPlugin: PluginObject<ToastOptions> = {
 	install(Vue, options) {
 		options = { ...options };
 
-		const Toast = new T(options);
+		const Toast = new Toasted(options);
 		Vue.component("toasted", ToastComponent);
 		Vue.toasted = Vue.prototype.$toasted = Toast;
-		Vue.toasts = Vue.prototype.$toasts = (id, message, options) => Toast.notify(id, message, options);
+		// Vue.toasts = Vue.prototype.$toasts = (id, message, options) => Toast.notify(id, message, options);
 		// Vue.config.globalProperties.$toasted = Toast;
 	},
 };
 
 // register plugin if it is used via cdn or directly as a script tag
 if (typeof window !== "undefined" && window.Vue) {
-	window.Toasted = Toasted;
+	window.Toasted = ToastedPlugin;
 }
 
-export default Toasted;
+export default ToastedPlugin;
