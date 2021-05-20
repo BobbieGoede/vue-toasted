9194a529c98ca23a8b216fd2d4731feeded76aef
Bobbie Goede <bobbiegoede@gmail.com>
2021-05-20 10:08:21 +0200

    Add module augmentations

---
+++ modules/vue.d.ts
@@ -0,0 +1,35 @@
+import Toasted from "../src/index-core";
+
+declare module "vue/types/vue" {
+	interface VueConstructor {
+		toasted: Toasted;
+	}
+
+	interface Vue {
+		$toasted: Toasted;
+	}
+}
+
+// Nuxt 2.9+
+declare module "@nuxt/types" {
+	interface Context {
+		$toast: Toasted;
+	}
+	interface NuxtAppOptions {
+		$toast: Toasted;
+	}
+}
+
+declare module "vue/types/vue" {
+	interface Vue {
+		$toast: Toasted;
+	}
+}
+
+declare module "vuex/types/index" {
+	interface Store<S> {
+		$toast: Toasted;
+	}
+}
+
+export {};
